e3795ae623e258640463fb694f07ae99b661550f
payelsky <68430680+payelsky@users.noreply.github.com>
2024-04-01 14:53:05 +0000

    setup project file

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import mongoose from "mongoose"
 import app from "./app.js"
+
+(async()=>{
+ try{
+  await mongoose.connect("mongodb://localhost:27017");
+  console.log("MongoDb Connected");
+  const onListening= ()=>{
+   console.log("Listening on Port 3000") 
+  }
+  app.listen(5000,onListening);
+ }catch(error){
+    console.error("error:",error)
+ }   
+})()
